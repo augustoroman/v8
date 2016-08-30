fa8977a0ab7cecad5078ebaf530bd72d33a5ebb0
Augusto Roman <aroman@gmail.com>
2016-08-30 16:58:54 -0700

    Add gclient sync after checking out the specific git tag.

--- install_v8_linux.sh
+++ install_v8_linux.sh
@@ -8,6 +8,7 @@ gclient
 fetch v8
 cd v8
 git checkout 5.4.374.1
+gclient sync
 make x64.release GYPFLAGS="-Dv8_use_external_startup_data=0 -Dv8_enable_i18n_support=0 -Dv8_enable_gdbjit=0"
 cd ../..
 ./symlink.sh chromium/v8
