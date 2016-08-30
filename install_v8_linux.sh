ccce99e39568a3dee64939693cfad75d2290a235
Augusto Roman <aroman@gmail.com>
2016-08-30 16:55:52 -0700

    Another attempt at getting travis to work.

---
+++ install_v8_linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+mkdir chromium
+cd chromium
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH="$(pwd)/depot_tools:$PATH"
+gclient
+fetch v8
+cd v8
+git checkout 5.4.374.1
+make x64.release GYPFLAGS="-Dv8_use_external_startup_data=0 -Dv8_enable_i18n_support=0 -Dv8_enable_gdbjit=0"
+cd ../..
+./symlink.sh chromium/v8
+go install .
