5cbccf7916d23d23a888a6cf8be8fa6183b37d57
Augusto Roman <aroman@gmail.com>
2016-08-30 20:16:15 -0700

    Hopefully final travis build attempt.
    Everything built and installed last time, but it tried to test the
    go files in the chromium dir.  Now, download and install chromium in
    a separate dir.

--- install_v8_linux.sh
+++ install_v8_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
-mkdir chromium
-cd chromium
+mkdir ${HOME}/chromium
+pushd ${HOME}/chromium
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH="$(pwd)/depot_tools:$PATH"
 gclient
@@ -10,6 +10,6 @@ cd v8
 git checkout 5.4.374.1
 gclient sync
 make x64.release GYPFLAGS="-Dv8_use_external_startup_data=0 -Dv8_enable_i18n_support=0 -Dv8_enable_gdbjit=0"
-cd ../..
-./symlink.sh chromium/v8
+popd
+./symlink.sh ${HOME}/chromium/v8
 go install .
