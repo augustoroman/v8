52dc303836e6cd875f8b6c26506decbea1a54c59
Augusto Roman <aroman@gmail.com>
2018-05-10 01:22:52 -0600

    Add a REPL to v8-runjs

--- cmd/v8-runjs/main.go
+++ cmd/v8-runjs/main.go
@@ -1,9 +1,10 @@
-// v8-runjs is a command-line tool to run javascript files.
+// v8-runjs is a command-line tool to run javascript.
 //
 // It's like node, but less useful.
 //
 // It runs the javascript files provided on the commandline in order until
-// it finishes or an error occurs.
+// it finishes or an error occurs. If no files are provided, this will enter a
+// REPL mode where you can interactively run javascript.
 //
 // Other than the standard javascript environment, it provides console.*:
 //   console.log, console.info: write args to stdout
@@ -15,11 +16,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 
 	"github.com/augustoroman/v8"
 	"github.com/augustoroman/v8/v8console"
+	"github.com/peterh/liner"
+)
+
+const (
+	kRESET = "\033[0m"
+	kRED   = "\033[91m"
 )
 
 func main() {
@@ -32,6 +41,25 @@ func main() {
 		failOnError(err)
 		_, err = ctx.Eval(string(data), filename)
 		failOnError(err)
+	}
+
+	if flag.NArg() == 0 {
+		s := liner.NewLiner()
+		defer s.Close()
+		for {
+			jscode, err := s.Prompt("> ")
+			if err == io.EOF {
+				break
+			}
+			failOnError(err)
+			s.AppendHistory(jscode)
+			result, err := ctx.Eval(jscode, "<input>")
+			if err != nil {
+				fmt.Println(kRED, err, kRESET)
+			} else {
+				fmt.Println(result)
+			}
+		}
 	}
 }
 
