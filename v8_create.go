b9efa696cbe59644da95f083d5afb4f18b568a2d
Augusto Roman <aroman@gmail.com>
2018-05-11 10:02:48 -0600

    Add -fpic to compile options since V8 uses PIC.
    In fact, PIC is the default for go and for v8, but not always the
    default for cgo? Not sure, but it fixes the occasional error and
    is clearly the desired state, so there you go.

--- v8_create.go
+++ v8_create.go
@@ -15,7 +15,7 @@ import (
 // #include <stdlib.h>
 // #include <string.h>
 // #include "v8_c_bridge.h"
-// #cgo CXXFLAGS: -I${SRCDIR} -I${SRCDIR}/include -fno-rtti -std=c++11
+// #cgo CXXFLAGS: -I${SRCDIR} -I${SRCDIR}/include -fno-rtti -fpic -std=c++11
 // #cgo LDFLAGS: -pthread -L${SRCDIR}/libv8 -lv8_base -lv8_init -lv8_initializers -lv8_libbase -lv8_libplatform -lv8_libsampler -lv8_nosnapshot
 import "C"
 
